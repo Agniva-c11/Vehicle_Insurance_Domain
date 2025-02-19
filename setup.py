65b1b3b1391d79dc4ab12ef80310d881d5ffef8c
Agniva <agnivac11@gmail.com>
2025-02-19 20:20:03 +0530

    Project structure setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author = "Agniva Chakravarty",
+    author_email="agnivac11@gmail.com",
+    packages=find_packages()
+)
